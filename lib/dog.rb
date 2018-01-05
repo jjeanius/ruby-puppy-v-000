cdd883dec46a70a1e8adac54153212e6e7a5d36a
Judy McGoldrick <jjeanius@verizon.net>
2018-01-05 04:44:53 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+
+  @@all =[]
+
+  def initialize(self)
+    @self = self
+    @@all << dog
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all?
+    @@all?
+  end
+
+  def new
+    @@new
+end
+
+def name
+  @@name
+end
+
+
+
+end
